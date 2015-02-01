69ca8281caa2af52302ac17e41aef46d25db06f2
Dan Flettre <fletd01@yahoo.com>
2015-02-01 14:33:57 -0600

    ready to rumble

---
+++ linter.py
@@ -0,0 +1,28 @@
+#
+# linter.py
+# Linter for SublimeLinter3, a code checking framework for Sublime Text 3
+#
+# Written by Dan Flettre
+# Copyright (c) 2015 Dan Flettre
+#
+# License: MIT
+#
+
+"""This module exports the Semistandard plugin class."""
+
+from SublimeLinter.lint import NodeLinter, util
+
+
+class Semistandard(NodeLinter):
+
+    """Provides an interface to semistandard."""
+
+    syntax = ('javascript', 'html', 'javascriptnext', 'javascript 6to5')
+    cmd = 'semistandard'
+    version_args = '--version'
+    version_re = r'(?P<version>\d+\.\d+\.\d+)'
+    version_requirement = '>= 2.3.1'
+    regex = r'^\s.+:(?P<line>\d+):(?P<col>\d+):(?P<message>.+)'
+    selectors = {
+        'html': 'source.js.embedded.html'
+    }
